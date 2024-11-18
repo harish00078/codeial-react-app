406ead1d79bfcc9840f6c22856f008b200f16cd9
harish kumar <harishpathania150@gmail.com>
2024-11-18 18:35:47 +0530

    updated the comment

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -23,7 +23,7 @@ import { PostsContext } from "../providers";
 // which we are importing from the (api) folder:
 // because we have written the same name for the other (login) or (login) handing function:
 // we can change its with the help of (as) keyword:
-// by using it we can change the name of (login) fucntion:
+// by using it we can change the name of (login) function:
 // when we are importing it from the (api) folder:
 // => 2 = here we are importing the (register) function:from the (api-folder):
 // => 3 = here we are importing the (editProfile):through which we gonna update the data of the (user-profile) on the (server):
@@ -36,9 +36,9 @@ import {
 } from "../api";
 
 // 1 = here we import the (setItemInLocalStorage) function:
-// through which we will bascially ADD the (token-value) of the (user-auth) in our (localStorage):
+// through which we will basically ADD the (token-value) of the (user-auth) in our (localStorage):
 // 2 = we also have import the (unique-key) from the (same) folder:
-// through which we will basically add the (token-value) in the lacalstorage:
+// through which we will basically add the (token-value) in the localstorage:
 // 3 = we will also import the (removeItemFromLocalStorage) function:
 // through which we will basically (remove) the (token-value) form the (localStorage):
 import {
@@ -54,14 +54,14 @@ import {
 // 2 = And with the help of (useAuth) custom-hook: we will be able to (access) that (Auth-state) of the (user) in our application components:
 
 export const useAuth = () => {
-  // V.IMP= 3  => so for giving the acccess of the (user-auth) state.
+  // V.IMP= 3  => so for giving the access of the (user-auth) state.
   //  with the help of this custom-hook function.In each and every component of our application:
   //  V.IMP=4  => we need to (return) the (auth-state) or (auth-context) of the (user) from this custom-hook function:
   //so that whenever we want to access the (auth-state) of the (user).
   // In our application components.we can have that with the help of this (useAuth) custom-hook:
 
   // V.IMP = 5=> the (Auth-state) of the (user).will be present in the(Auth-context).which we have created:
-  // so we have to (return) that (Auth-context):form this custom-hook fucntion:
+  // so we have to (return) that (Auth-context):form this custom-hook function:
   // so that we can have the (access) of that (Auth-context or state) of the (user) in our each and every component of application:
   // V.IMP = 6 => so for doing that first we need to have that (Auth-context) of the (user) in this (custom-hook) function:
   // we can have that (Auth-context)  in this custom-hook:
@@ -71,7 +71,7 @@ export const useAuth = () => {
   // V.IMP = 7 => the main reason of doing this or creating this (custom-hook) function:
   // so that we did not have to import the (useContext) hook again and again.
   // were we want to have the access of user (Auth-state or Context):
-  // we can simple get that state with the help of this (custom-hook) funciton:
+  // we can simple get that state with the help of this (custom-hook) function:
 
   return useContext(AuthContext);
 };
@@ -82,7 +82,7 @@ export const useAuth = () => {
 // here we are using the (arrow-function):
 export const useProvideAuth = () => {
   // 1 => under this custom-hook or function.we  basically have or handle the (user-auth) state:
-  // that (state) basically handle or manage by the inbuild  (useState) hook:
+  // that (state) basically handle or manage by the in build  (useState) hook:
   // by default it has a value of (null):because by default our user have a (null) auth:
   const [user, setUser] = useState(null);
   // 2 => this function also have a (loading) functionality in it:
